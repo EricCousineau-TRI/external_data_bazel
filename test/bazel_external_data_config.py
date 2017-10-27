d9c092313d662ed0632ff51da5e164e93a1334aa
Eric Cousineau <eric.cousineau@tri.global>
2017-10-27 11:25:31 -0400

    Add mock cache

--- test/bazel_external_data_config.py
+++ test/bazel_external_data_config.py
@@ -6,7 +6,7 @@ from bazel_external_data import util
 class CustomSetup(util.ProjectSetup):
     def get_config(self, filepath):
         sentinel = {'file': '.custom-sentinel'}
-        return util.ProjectSetup.get_config(self, filepath, sentinel=sentinel)
+        config = util.ProjectSetup.get_config(self, filepath, sentinel=sentinel)
 
     def get_backends(self):
         backends = util.ProjectSetup.get_backends()
