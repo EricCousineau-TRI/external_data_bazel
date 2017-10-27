aecd3e41ac5a56e02467ef5776e740053524c745
Eric Cousineau <eric.cousineau@tri.global>
2017-10-27 18:32:39 -0400

    Add --keep_going.

--- src/bazel_external_data/download.py
+++ src/bazel_external_data/download.py
@@ -18,16 +18,18 @@ assert __name__ == '__main__'
 # TODO(eric.cousineau): Ensure that we do not need immediate authentication in configuration, e.g. when in road warrior mode.
 
 parser = argparse.ArgumentParser()
+parser.add_argument('-o', '--output', dest='output_file', type=str,
+                    help='Output destination. If specified, only one input file may be provided.')
+parser.add_argument('-k', '--keep_going', action='store_true',
+                    help='Attempt to keep going.')
+parser.add_argument('-f', '--force', action='store_true',
+                    help='Overwrite existing output file.')
 parser.add_argument('--no_cache', action='store_true',
                     help='Always download, and do not cache the result.')
 parser.add_argument('--symlink_from_cache', action='store_true',
                     help='Use a symlink from the cache rather than copying the file.')
 parser.add_argument('--allow_relpath', action='store_true',
                     help='Permit relative paths. Having this on by default makes using Bazel simpler.')
-parser.add_argument('-f', '--force', action='store_true',
-                    help='Overwrite existing output file if it already exists.')
-parser.add_argument('-o', '--output', dest='output_file', type=str,
-                    help='Output destination. If specified, only one input file may be provided.')
 parser.add_argument('--remote', type=str, default=None,
                     help='Configuration defining a custom override remote. Useful for direct, single-file downloads.')
 parser.add_argument('--debug_config', action='store_true',
@@ -97,4 +99,13 @@ if args.output_file:
 else:
     for sha_file in args.sha_files:
         output_file = sha_file[:-len(SHA_SUFFIX)]
-        do_download(project, sha_file, output_file, remote_in=remote_in)
+        def action():
+            do_download(project, sha_file, output_file, remote_in=remote_in)
+        if args.keep_going:
+            try:
+                action()
+            except Exception as e:
+                util.eprint(e)
+                util.eprint("Continuing (--keep_going).")
+        else:
+            action()
