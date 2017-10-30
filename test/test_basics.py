6b8cc74a254efd3a2e680db6abd313e2d39b063b
Eric Cousineau <eric.cousineau@tri.global>
2017-10-30 19:49:31 -0400

    Check expected downstream utility behavior.

--- test/test_basics.py
+++ test/test_basics.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
 import os
+from bazel_external_data.util import subshell
 
 expected_files = {
     "backend_root": [
         "basic.bin",
-        "direct.bin",
         "glob_1.bin",
         "glob_2.bin",
         "glob_3.bin",
@@ -13,18 +13,25 @@ expected_files = {
     "backend_child": [
         "extra.bin",
     ],
+    None: [
+        "direct.bin",
+    ],
 }
 
 data_dir = 'test/data'
 mock_dir = 'test/mock'
 
 # Go through each file and ensure that we have the desired contents.
-for file in find_files('*.bin'):
+files = subshell("find test/data -name '*.bin'")
+for file in files.split('\n'):
     contents = open(file).read()
     file_name = os.path.basename(file)
     for mock_name, mock_file_names in expected_files.iteritems():
         if file_name in mock_file_names:
-            mock_file = os.path.join(mock_dir, mock_name, file_name)
-            mock_contents = open(mock_file).read()
+            if mock_name is not None:
+                mock_file = os.path.join(mock_dir, mock_name, file_name)
+                mock_contents = open(mock_file).read()
+            else:
+                mock_contents = "Content for '{}'\n".format(file_name)
             break
     assert contents == mock_contents
