e979d5e66a196aa94198f9673a2db19b45947809
Eric Cousineau <eric.cousineau@tri.global>
2017-11-01 09:48:24 -0400

    Test basic workflow

--- test_simple/test_basics.py
+++ test_simple/test_basics.py
@@ -1,36 +1,10 @@
 #!/usr/bin/env python
 
 import os
-from bazel_external_data.util import subshell
 
-expected_files = {
-    "backend_root": [
-        "basic.bin",
-        "glob_1.bin",
-        "glob_2.bin",
-        "glob_3.bin",
-    ],
-    "backend_child": [
-        "extra.bin",
-    ],
-    "backend_url": [
-        "direct.bin",
-    ],
-}
+with open('data/direct.bin') as f:
+    contents = f.read()
+contents_expected = "Content for 'direct.bin'\n"
+assert contents == contents_expected
 
-data_dir = 'data'
-mock_dir = 'mock'
-
-# Go through each file and ensure that we have the desired contents.
-files = subshell("find data -name '*.bin'")
-for file in files.split('\n'):
-    contents = open(file).read()
-    file_name = os.path.basename(file)
-
-    mock_contents = None
-    for mock_name, mock_file_names in expected_files.iteritems():
-        if file_name in mock_file_names:
-            mock_file = os.path.join(mock_dir, mock_name, file_name)
-            mock_contents = open(mock_file).read()
-            break
-    assert contents == mock_contents
+print("Expected contents")
